94fe805d12945a92f0e223cc2b30e77a19b66e59
sean <seanep94@gmail.com>
2022-08-27 11:40:07 -0700

    Added Readme, prepped other computer for coding

---
+++ README.md
@@ -0,0 +1,15 @@
+This is the code base for my resume website.
+
+This is my first largescale website, beyond practice and minor projects.
+Using Flask to run the website, and designing it with HTML, CSS, and JS.
+
+Expectations/Plans:
+Initial
+Into page: About me
+Link to Resume: Build resume with Html/Css
+Prep website for links to other projects.
+
+Post Launch:
+Prep pages for Data analytics projects/Basically turn portion of site into showcase for Data Analyst projects. (like a DataScience App)
+Working on Pico8 game, once complete launch a link to the game(s), plus a small blog going over how I went about building game (Lua based retro game engine)
+
--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 '''
 Preweekend final prep:
-***DO NOT CONTINUE CODING THIS FILE, WITHOUT A GITHUB REPO***
-
 Flask:
     Keep familiarizing.
 
@@ -14,22 +12,15 @@ Pico8: #Going to include also weekend work as well...
         If standing under a long wall, if I jump at the right pixel itll clip into the wall. Need to fix.
             --Fix AFTER the xforce is working. Because it's probably a variable issue, so if I get xforce
                 working first, then the bug should stay fixed after I fix it.
-
 Html&CSS:
     Pick a good online website template.
     Pick a color pallet
-
-    Try to finish a CSS course and intermediate CSS on Codecademy.
-
-JS:
-    g2g
-
+    
 Weekend Goal:
     Have SOMETHING that looks like an okay website.
-    Use Codepen:
-
-
+    Use Codepen to troubleshoot ideas.
 '''
+
 from flask import Flask # And templates..
 app = Flask(__name__)
 
